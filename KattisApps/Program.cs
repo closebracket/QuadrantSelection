97cbf0dfddcf72e0367a4e4e126076c8856c3765
closebracket <ec557782@gmail.com>
2020-06-17 19:33:22 +0100

    Add project files.

---
+++ KattisApps/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace KattisApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FinalValue = 0;
+            int X = int.Parse(Console.ReadLine());
+            int Y =  int.Parse(Console.ReadLine());
+
+            if (X > 0 && Y > 0)
+            {
+                FinalValue = 1;
+            }
+            else if (X < 0 && Y > 0)
+            {
+                FinalValue = 2;
+            }
+           else if (X < 0 && Y < 0)
+            {
+                FinalValue = 3;
+            }
+           else if (X > 0 && Y < 0)
+            {
+                FinalValue = 4;
+            }
+
+            Console.WriteLine(FinalValue);
+            
+        }
+    }
+}
